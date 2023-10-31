94b9631073fd08073a3681cd6a859b72a15d320d
Zhandos Ospanov <zhanazhan@gmail.com>
2023-10-31 10:08:22 +0600

    add postman.

--- aws-frontend/src/constants/api-paths.ts
+++ aws-frontend/src/constants/api-paths.ts
@@ -1,7 +1,6 @@
 export const API_PATHS = {
-	product:
-		'https://k5kg99mnp9.execute-api.us-east-1.amazonaws.com/dev/products',
+	product: 'https://k5kg99mnp9.execute-api.us-east-1.amazonaws.com/dev/products',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://dkv6hpmwp4.execute-api.us-east-1.amazonaws.com/dev/import',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
